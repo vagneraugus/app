a9f057380f33690dfa53234d16ed81dfd813b832
Robson Cominato <robson.cominato@gmail.com>
2018-03-09 21:47:20 -0300

    Tela esqueceu senha e ligar com login - layout

--- confin/app/src/main/java/com/confin/confin/Cadastro.java
+++ confin/app/src/main/java/com/confin/confin/Cadastro.java
@@ -1,7 +1,11 @@
 package com.confin.confin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        final TextView text = findViewById(R.id.txtEsqueceuSenha);
+        text.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Cadastro.this, EsqueceuSenha.class));
+            }
+        });
+
+        final Button button = findViewById(R.id.btnCadastrar);
+        button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(new Intent(Cadastro.this, LoginActivity.class));
+                }
+        });
     }
 }
