850384404bc5748a9ecb20e99e2e423ad58314f7
vagneraugus <vagneraugus@gmail.com>
2018-03-19 17:00:06 -0300

    Modificado o menu de cadastro usado if else para switch

--- confin/app/src/main/java/com/confin/confin/MainActivity.java
+++ confin/app/src/main/java/com/confin/confin/MainActivity.java
@@ -22,123 +22,128 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private AlertDialog.Builder opcaoCadastrar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CharSequence[] opcoesCadastro = {
-                "CATEGORIAS",
-                "CONDIÇÃO DE PGTO",
-                "DESPESAS",
-                "RECEITAS",
-                "SUBCATEGORIAS"
-        };
-        opcaoCadastrar = new AlertDialog.Builder(MainActivity.this);
-        opcaoCadastrar.setTitle("O que você deseja cadastrar?");
-        opcaoCadastrar.setCancelable(false);
-        opcaoCadastrar.setItems(opcoesCadastro, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int selecionado) {
-                if (opcoesCadastro[selecionado].equals("CATEGORIAS")){
-                    startActivity(new Intent(MainActivity.this, Categorias.class));
-                }else
-                    if (opcoesCadastro[selecionado].equals("CONDIÇÃO DE PGTO")){
-//                    startActivity(new Intent(MainActivity.this, Categorias.class));
-                    Toast.makeText(getApplicationContext(),"Condição de PGTO selecionado", Toast.LENGTH_LONG).show();
-                }else
-                    if (opcoesCadastro[selecionado].equals("DESPESAS")){
-                    startActivity(new Intent(MainActivity.this, Despesas.class));
-                }else
-                    if (opcoesCadastro[selecionado].equals("RECEITAS")){
-                    startActivity(new Intent(MainActivity.this, Receitas.class));
-                }else
-                    if (opcoesCadastro[selecionado].equals("SUBCATEGORIAS")){
-//                    startActivity(new Intent(MainActivity.this, Subcategorias.class));
-                    Toast.makeText(getApplicationContext(),"subcategorias selecionado", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-        opcaoCadastrar.setNegativeButton("Cancelar", null);
+        //---------------------------- MENU DE CADASTROS ALERT_DIALOG ------------------------------
+            final CharSequence[] opcoesCadastro = { "CATEGORIAS", "CONDIÇÃO DE PGTO", "DESPESAS", "RECEITAS", "SUBCATEGORIAS" };
+
+            opcaoCadastrar = new AlertDialog.Builder(MainActivity.this);
+            opcaoCadastrar.setTitle("O que você deseja cadastrar?");
+            opcaoCadastrar.setNegativeButton("Cancelar", null);
+            opcaoCadastrar.setCancelable(false);
+            opcaoCadastrar.setItems(opcoesCadastro, new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int selecionado)
+                    {
+                        switch (selecionado)
+                        {
+                            case 0:
+                                startActivity(new Intent(MainActivity.this, Categorias.class));
+                                break;
+                            case 1:
+                                // startActivity(new Intent(MainActivity.this, CondPgto.class));
+                                Toast.makeText(getApplicationContext(),"Condição de PGTO selecionado", Toast.LENGTH_LONG).show();
+                                break;
+                            case 2:
+                                startActivity(new Intent(MainActivity.this, Despesas.class));
+                                break;
+                            case 3:
+                                startActivity(new Intent(MainActivity.this, Receitas.class));
+                                break;
+                            case 4:
+                                // startActivity(new Intent(MainActivity.this, Categorias.class));
+                                Toast.makeText(getApplicationContext(),"Condição de PGTO selecionado", Toast.LENGTH_LONG).show();
+                                break;
+                        }
+                    }
+            });
+        //------------------------------------------------------------------------------------------
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //-----------------------------------BOTAO FLUTUANTE DE ADD --------------------------------
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                    {
+                        opcaoCadastrar.create().show();
+                    }
+            });
+        //----------------------------------- FIM BOTAO FLUTUANTE DE ADD ---------------------------
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+    }
+
             @Override
-            public void onClick(View view) {
-                opcaoCadastrar.create().show();
+            public void onBackPressed() {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    super.onBackPressed();
+                }
             }
-        });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu)
+                {
+                    // Inflate the menu; this adds items to the action bar if it is present.
+                    getMenuInflater().inflate(R.menu.main, menu);
+                    return true;
+                }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item)
+            {
+                // Handle action bar item clicks here. The action bar will
+                // automatically handle clicks on the Home/Up button, so long
+                // as you specify a parent activity in AndroidManifest.xml.
+                int id = item.getItemId();
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+                //noinspection SimplifiableIfStatement
+                if (id == R.id.action_settings) {
+                    return true;
+                }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+                return super.onOptionsItemSelected(item);
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //---------------------------------- MENU LATERAL FLUTUANTE DE ADD -----------------------------
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item)
+            {
+                int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                if (id == R.id.nav_cadastrar) {
+                    opcaoCadastrar.create().show();
+                } else if (id == R.id.nav_despesas) {
+                    startActivity(new Intent(MainActivity.this, Despesas.class));
+                } else if (id == R.id.nav_receitas) {
+                    startActivity(new Intent (MainActivity.this, Receitas.class));
+                } else if (id == R.id.nav_resumo) {
 
-        return super.onOptionsItemSelected(item);
-    }
+                } else if (id == R.id.nav_compartilhar) {
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+                } else if (id == R.id.nav_enviar) {
+                }
 
-        if (id == R.id.nav_cadastrar) {
-//            startActivity(new Intent(MainActivity.this, Cadastros.class));
-            opcaoCadastrar.create().show();
-
-        } else if (id == R.id.nav_despesas) {
-            startActivity(new Intent(MainActivity.this, Despesas.class));
-        } else if (id == R.id.nav_receitas) {
-            startActivity(new Intent (MainActivity.this, Receitas.class));
-        } else if (id == R.id.nav_resumo) {
-
-        } else if (id == R.id.nav_compartilhar) {
-
-        } else if (id == R.id.nav_enviar) {
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    public void dd(MenuItem item) {
-        Toast.makeText(getApplicationContext(),"testes dfksdjflj",Toast.LENGTH_LONG).show();
-    }
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+    //----------------------------------- FIM MENU LATERAL FLUTUANTE DE ADD ------------------------
 }
